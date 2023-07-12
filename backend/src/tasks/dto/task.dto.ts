c07b90b62c5ccda346226b4f0c6f2d92d8484fd0
Federico Gonzalez <federicogonz2016@gmail.com>
2023-07-12 20:55:37 -0300

    FEATURE: Creados todos endpoints y servicios

--- backend/src/tasks/dto/task.dto.ts
+++ backend/src/tasks/dto/task.dto.ts
@@ -1,6 +1,14 @@
-//task.dto.ts
+import { TaskStatus } from "../task.entity"
 
+//task.dto.ts
+// SIRVE PARA DETALLAR LOS DATOS QUE ESTAN LLEGANDO AL BACKEND
 export class CreateTaskDto {
     title: string
     description: string
+}
+
+export class UpdateTaskDto {
+    title?: string // EL SIGNO DE INTERROGACION SIGNIFICA QUE EL ARGUMENTO ES OPCIONAL
+    description?: string
+    status?: TaskStatus
 }
--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -2,10 +2,12 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.entity';
 import {v4} from 'uuid';
+import { UpdateTaskDto } from './dto/task.dto';
 
 @Injectable()
 export class TasksService {
 
+  // Simula una base de datos  
   private tasks: Task[] = 
   [
     {
@@ -20,9 +22,9 @@ export class TasksService {
         return this.tasks;
     }
 
-  // Resto de los métodos
+     // Resto de los métodos
 
-    //getTaskById() {}
+    
     createTask(title: string, description: string) {
        const task = {
         id: v4(),
@@ -34,6 +36,27 @@ export class TasksService {
 
         return task
     }
-    updateTask() {}
-    deleteTask() {}
+    
+    deleteTask(id : string) {
+        // MIENTRAS LA TAREA SEA DIFERENTE A LA QUE LE PASO POR PARAMETRO, QUE ME LA GUARDE EN "tasks"
+        this.tasks = this.tasks.filter(task => task.id !== id)
+    }
+
+    getTaskById(id : string) {
+
+        // DEVUELVEME UNA TAREA EN LA QUE COINCIDA EL ID INDICADO
+        return this.tasks.find (task => task.id === id)
+    }
+
+    updateTask(id : string, updatedFields: UpdateTaskDto): Task {
+        // BUSCAMOS TAREA POR ID
+        const task = this.getTaskById(id)
+        //( COMBINAMOS )UTILIZAMOS UNA PROPIEDAD DE OBJETOS DE JAVASCRIPT LLAMADA "assign" PARA UNIR LO QUE TENIAMOS ANTES MAS LO QUE LE ESTAMOS PASANDO POR PARAMETROS, Y EN CASO QUE SE REPITAN LOS CAMPOS SE SOBREESCRIBEN.
+        const newTask = Object.assign(task, updatedFields)
+        // RECORREMOS Y ACTUALIZAMOS
+        this.tasks = this.tasks.map(task => task.id === id ? newTask : task)
+
+        return newTask;
+    }
+
 }
